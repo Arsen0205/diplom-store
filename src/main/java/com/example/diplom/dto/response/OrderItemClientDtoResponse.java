954c15234f703f4606e0f15acfeb4d535d6017ed
Zoro <arsen.murtazayev@inbox.ru>
2025-03-19 15:51:20 +0300

    Added a detailed order review

---
+++ src/main/java/com/example/diplom/dto/response/OrderItemClientDtoResponse.java
@@ -0,0 +1,14 @@
+package com.example.diplom.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class OrderItemClientDtoResponse {
+    private String title;
+    private int quantity;
+    private BigDecimal sellingPrice;
+}
