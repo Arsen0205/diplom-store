b21ed6d947a842f9f760b71d0192f2373c8fe5e6
Zoro <arsen.murtazayev@inbox.ru>
2025-04-13 22:42:31 +0300

    Returned JSON, not strings in AuthController

--- src/main/java/com/example/diplom/controller/AuthController.java
+++ src/main/java/com/example/diplom/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.example.diplom.controller;
 
 import com.example.diplom.dto.request.LoginDtoRequest;
 import com.example.diplom.dto.request.RegisterDtoRequest;
+import com.example.diplom.dto.response.UserInfoDtoResponse;
 import com.example.diplom.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -16,15 +17,15 @@ public class AuthController {
 
     //Регистрация нового пользователя
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@Valid @RequestBody RegisterDtoRequest request){
-        authService.register(request);
-        return ResponseEntity.ok("Вы успешно зарегистрировались!");
+    public ResponseEntity<UserInfoDtoResponse> registerUser(@Valid @RequestBody RegisterDtoRequest request){
+        UserInfoDtoResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
     }
 
     //Авторизация пользователя
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@Valid @RequestBody LoginDtoRequest request){
-        authService.login(request);
-        return ResponseEntity.ok("Вы успешно вошли");
+    public ResponseEntity<UserInfoDtoResponse> loginUser(@Valid @RequestBody LoginDtoRequest request){
+        UserInfoDtoResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
     }
 }
